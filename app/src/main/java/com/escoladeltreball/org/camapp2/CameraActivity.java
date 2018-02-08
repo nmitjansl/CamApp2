9f9353e3ef4e5d5d7bef2a20ee096bee967c55c2
arodriguezq <aitorrq@gmail.com>
2018-02-08 21:06:39 +0100

    CameraActivity finished

--- app/src/main/java/com/escoladeltreball/org/camapp2/CameraActivity.java
+++ app/src/main/java/com/escoladeltreball/org/camapp2/CameraActivity.java
@@ -8,24 +8,14 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
-
-import com.escoladeltreball.org.camapp2.api.firebase.FirebaseConnection;
 
 import java.io.File;
-import java.io.IOError;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
 
-    /*static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_TAKE_PHOTO = 1;
-    String mCurrentPhotoPath;*/
 
     private static final String GALLERY = "/CamApp2";
-    private File imgFile;
+    private Uri imgUri;
     private static final int MY_REQUEST_CODE = 12;
 
     private static final String LOG_TAG = "CamAPP2Log";
@@ -35,12 +25,11 @@ public class CameraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        //dispatchTakePictureIntent();
-
         openCamera();
-        if(imgFile != null){
-            //FirebaseConnection.upload(imgFile.getPath());
+        if(imgUri != null){
+            //FirebaseConnection.upload(imgUri);
         }
+        finish();
 
     }
 
@@ -68,81 +57,10 @@ public class CameraActivity extends AppCompatActivity {
             Log.e(LOG_TAG, "Directory not created");
         }
         File image = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + GALLERY, System.currentTimeMillis() + ".jpeg");
-        imgFile = image;
+        Uri uri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", image);
+        imgUri = uri;
 
-        Uri imgUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", image);
-
-        return imgUri;
+        return uri;
     }
-
-
-
-    /*private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (photoFile != null) {
-                Log.v("BLABLA", photoFile.toString());
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-            //startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-
-    }
-
-
-    // Crea un File per guardar la foto en ell
-    private File createImageFile() throws IOException {
-        //String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String timeStamp = "PEPITO";
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_PICTURES), "CamApp2ALBUM");
-
-        File image = File.createTempFile(
-                imageFileName,  *//* prefix *//*
-                ".jpg",         *//* suffix *//*
-                storageDir      *//* directory *//*
-        );
-
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            galleryAddPic();
-            Log.v("BLABLA", "CREAT!");
-        }
-        Log.v("BLABLA", "FORA!");
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        galleryAddPic();
-    }
-
-
-
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(mCurrentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
-    }*/
 
 }
