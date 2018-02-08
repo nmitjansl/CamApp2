f747fbf806f7bfc2cbd3d457b32634b8d0d18cfe
arodriguezq <aitorrq@gmail.com>
2018-02-08 20:52:47 +0100

    Intent to camera finished

--- app/src/main/java/com/escoladeltreball/org/camapp2/CameraActivity.java
+++ app/src/main/java/com/escoladeltreball/org/camapp2/CameraActivity.java
@@ -18,23 +18,60 @@ import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    /*static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_PHOTO = 1;
-    String mCurrentPhotoPath;
+    String mCurrentPhotoPath;*/
 
+    private static final String GALLERY = "/CamApp2";
+    private static final int MY_REQUEST_CODE = 12;
+    private static final int TAKE_PICTURE = 1;
+
+    private static final String LOG_TAG = "CamAPP2Log";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        dispatchTakePictureIntent();
+        //dispatchTakePictureIntent();
 
-        // TODO feedback using Toast
+        openCamera();
+
+    }
+
+    private void openCamera() {
+        try {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, getImageUri());
+
+            startActivityForResult(cameraIntent, MY_REQUEST_CODE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Uri getImageUri() {
+
+
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) + GALLERY);
+        if (!file.exists()) {
+            file.mkdirs();
+            Log.e(LOG_TAG, "Directory created");
+        } else {
+            Log.e(LOG_TAG, "Directory not created");
+        }
+        File file2 = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + GALLERY, System.currentTimeMillis() + ".jpeg");
+
+        Uri imgUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", file2);
+
+        return imgUri;
     }
 
 
-    private void dispatchTakePictureIntent() {
+
+    /*private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             File photoFile = null;
@@ -67,9 +104,9 @@ public class CameraActivity extends AppCompatActivity {
             Environment.DIRECTORY_PICTURES), "CamApp2ALBUM");
 
         File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
+                imageFileName,  *//* prefix *//*
+                ".jpg",         *//* suffix *//*
+                storageDir      *//* directory *//*
         );
 
         mCurrentPhotoPath = image.getAbsolutePath();
@@ -100,6 +137,6 @@ public class CameraActivity extends AppCompatActivity {
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
-    }
+    }*/
 
 }
